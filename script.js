b42688bf991e2a1f06db88088fb3f2e5a1a64fbc
Shashankreddy-Techie <152247010+Shashankreddy-Techie@users.noreply.github.com>
2024-03-05 07:20:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const wrapper = document.querySelector(".wrapper"),
-qrInput = wrapper.querySelector(".form input"),
-generateBtn = wrapper.querySelector(".form button"),
-qrImg = wrapper.querySelector(".qr-code img");
+const wrapper = document.querySelector("wrapper"),
+qrInput = wrapper.querySelector("form input"),
+generateBtn = wrapper.querySelector("form button"),
+qrImg = wrapper.querySelector("qr-code img");
 
 generateBtn.addEventListener("click", ()=>{
     let qrValue = qrInput.value;
